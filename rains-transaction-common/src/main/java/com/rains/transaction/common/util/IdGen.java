a617ebc649067f8d28a53fc360af7627c12ea85c
hugoDD <dou@305>
2018-11-08 08:48:30 +0800

    auto config

---
+++ rains-transaction-common/src/main/java/com/rains/transaction/common/util/IdGen.java
@@ -0,0 +1,11 @@
+package com.rains.transaction.common.util;
+
+/*
+ * 文 件 名:  IdGen
+ * 版    权:  Copyright (c) 2018 com.rains.hugosz
+ * 描    述:  todo
+ * 创 建 人:  hugosz
+ * 创建时间:  2018年 11 月  06日  16:17
+ */
+public class IdGen {
+}
